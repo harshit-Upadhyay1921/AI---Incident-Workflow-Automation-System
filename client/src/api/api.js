e948c79d7d031027e913ee53e293efec48931f5b
harshit-Upadhyay1921 <harshitupadhyay1921@gmail.com>
2026-01-05 16:13:31 +0530

    updated api.js file

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: '/api',
-    withCredentials: true
-})
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+});
 
 export default api;
